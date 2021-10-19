2d74334fc1e8d71ec6e2d96822bd61295bcd2d0a
Boba Fett <thomascupchurch@gmail.com>
2021-10-19 16:47:37 -0500

    tests complete for About and Nav

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -23,18 +23,17 @@ function Nav() {
   return (
     <header data-testid="header" className="flex-row px-1">
       <h2>
-        <a href="/">
+        <a data-testid="link" href="/">
           <span role="img" aria-label="camera">
-            {" "}
             📸
-          </span>{" "}
+          </span>
           Oh Snap!
         </a>
       </h2>
       <nav>
         <ul className="flex-row">
           <li className="mx-2">
-            <a href="#about" onClick={() => handleClick()}>
+            <a data-testid="about" href="#about" onClick={() => handleClick()}>
               About me
             </a>
           </li>
