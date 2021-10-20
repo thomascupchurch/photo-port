8a4dd488af57d428c8c93d4ec4798d54e88802f4
Boba Fett <thomascupchurch@gmail.com>
2021-10-20 11:19:03 -0500

    complete Gallery component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Nav from "./components/Nav";
 import About from "./components/About";
+import Gallery from "./components/Gallery";
 
 function App() {
   return (
     <div>
       <Nav />
       <main>
+        <Gallery />
         <About />
       </main>
     </div>
