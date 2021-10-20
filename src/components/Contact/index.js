6bf7876482369aa30b2bdbf808322c23c0679ffa
Boba Fett <thomascupchurch@gmail.com>
2021-10-20 17:27:16 -0500

    error messages working

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
+import { validateEmail } from "../../utils/helpers";
 
 function ContactForm() {
+  const [errorMessage, setErrorMessage] = useState("");
   const [formState, setFormState] = useState({
     name: "",
     email: "",
@@ -8,7 +10,25 @@ function ContactForm() {
   });
   const { name, email, message } = formState;
   function handleChange(e) {
-    setFormState({ ...formState, [e.target.name]: e.target.value });
+    if (e.target.name === "email") {
+      const isValid = validateEmail(e.target.value);
+      console.log(isValid);
+      // isValid conditional statement
+      if (!isValid) {
+        setErrorMessage("Your email is invalid.");
+      } else {
+        setErrorMessage("");
+      }
+    } else {
+      if (!e.target.value.length) {
+        setErrorMessage(`${e.target.name} is required.`);
+      } else {
+        setErrorMessage("");
+      }
+    }
+    if (!errorMessage) {
+      setFormState({ ...formState, [e.target.name]: e.target.value });
+    }
   }
   function handleSubmit(e) {
     e.preventDefault();
