22404af784762899aa192ce6acadd32bd9e330cc
Boba Fett <thomascupchurch@gmail.com>
2021-10-20 12:43:28 -0500

    add navActive state

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -2,7 +2,8 @@ import React from "react";
 import { capitalizeFirstLetter } from "../../utils/helpers";
 
 function Nav() {
-  const categories = [
+  const [currentCategory, setCurrentCategory] = useState(categories[0]);
+  const [categories] = useState([
     {
       name: "commercial",
       description:
@@ -14,37 +15,37 @@ function Nav() {
       name: "landscape",
       description: "Fields, farmhouses, waterfalls, and the beauty of nature",
     },
-  ];
-
-  const handleClick = () => {
-    console.log("click handled");
-  };
+  ]);
 
   return (
-    <header data-testid="header" className="flex-row px-1">
+    <header className="flex-row px-1">
       <h2>
         <a data-testid="link" href="/">
           <span role="img" aria-label="camera">
+            {" "}
             📸
-          </span>
+          </span>{" "}
           Oh Snap!
         </a>
       </h2>
       <nav>
         <ul className="flex-row">
           <li className="mx-2">
-            <a data-testid="about" href="#about" onClick={() => handleClick()}>
-              About me
-            </a>
+            <a href="#about">About me</a>
           </li>
-          <li className={"mx-2"}>
-            <span onClick={() => handleClick()}>Contact</span>
+          <li>
+            <span>Contact</span>
           </li>
           {categories.map((category) => (
-            <li className="mx-1" key={category.name}>
+            <li
+              className={`mx-1 ${
+                currentCategory.name === category.name && "navActve"
+              }`}
+              key={category.name}
+            >
               <span
                 onClick={() => {
-                  handleClick();
+                  setCurrentCategory(category);
                 }}
               >
                 {capitalizeFirstLetter(category.name)}
